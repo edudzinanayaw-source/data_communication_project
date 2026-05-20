a3af0b36577a3e3fbb3a28f031c145b96b78e636
edudzinanayaw-source <edudzinanayaw@gmail.com>
2026-05-20 03:59:15 +0000

    Add files via upload

---
+++ datacomm/script.js
@@ -0,0 +1,45 @@
+// =========================
+//        script.js
+// =========================
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+/* =========================
+        SCROLL ANIMATION
+========================= */
+
+const cards = document.querySelectorAll(
+".topic-card, .about-card, .resource-card"
+);
+
+window.addEventListener("scroll", () => {
+
+    cards.forEach(card => {
+
+        const cardTop = card.getBoundingClientRect().top;
+
+        if(cardTop < window.innerHeight - 100){
+
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+
+        }
+
+    });
+
+});
+
+cards.forEach(card => {
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
+    card.style.transition = "0.6s ease";
+
+});
